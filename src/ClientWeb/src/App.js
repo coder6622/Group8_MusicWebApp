894df86f81f868ba9cff4d145e073ad9606faac3
coder6622 <tranngochoanglong469@gmail.com>
2023-04-03 20:05:00 +0700

    Initial ✔️✔️🍀🍀🍀

--- src/ClientWeb/src/App.js
+++ src/ClientWeb/src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div className='App'>
-      <h1>Hello team</h1>
+      <h1>Hello world</h1>
+      <button className='btn btn-primary'> Hello world</button>
     </div>
   );
 }
--- src/ClientWeb/src/index.js
+++ src/ClientWeb/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GlobalStyles from './components/GlobalStyles';
 import App from './App';
-import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root'),
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
   </React.StrictMode>,
 );
