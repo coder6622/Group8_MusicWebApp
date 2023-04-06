ad3cbea879574cd74b5c1c5116721c6ee2a1cd69
TranTrungHieu02 <hieupor002@gmail.com>
2023-04-06 21:08:17 +0700

    Loi nhieu qua bann :DDD

---
+++ src/Server/MusicApp.Services/Musics/Artists/IArtistsRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicApp.Core.Contracts;
+using MusicApp.Core.Entities;
+using MusicApp.Core.DTO;
+
+namespace MusicApp.Services.Musics.Artists
+{
+    public interface IArtistsRepository
+    {
+        Task<bool> AddOrUpdateArtists(
+            Artist artist,
+            CancellationToken cancellationToken = default);
+
+        public Task<bool> DeleteArtists(
+            Guid id,
+            CancellationToken cancellationToken = default);
+
+        Task<Artist> GetArtistByIdAsync(Guid artistId);
+
+        Task<Artist> GetCachedArtistByIDAsync(Guid artistId);
+
+        Task<Artist> GetArtistBySlugAsync(
+            string slug, CancellationToken cancellationToken = default);
+
+        Task<Artist> GetCacheArtistBySlugAsync(
+            string slug, CancellationToken cancellationToken = default);
+
+        Task<IList<ArtistItem>> GetArtistAsync (CancellationToken cancellationToken = default);
+
+
+    }
+}
---
+++ src/Server/MusicApp.WebApi/Validations/ArtistValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using MusicApp.WebApi.Models.Artist;
+
+namespace MusicApp.WebApi.Validations
+{
+    public class ArtistValidator : AbstractValidator<ArtistEditModel>
+    {
+        public ArtistValidator() 
+        {
+            RuleFor(a => a.FullName)
+                .NotEmpty()
+                .WithMessage("Tên Tác giả/ca sĩ không được để trống!!!!!")
+                .MaximumLength(100)
+                .WithMessage("Tên tác giả tối đa 100 ký tự :3");
+
+            RuleFor(a => a.UrlSlug)
+                .NotEmpty()
+                .WithMessage("UrlSlug không được để trống!!!!!")
+                .MaximumLength(100)
+                .WithMessage("UrlSlug tối đa 100 ký tự :3");
+
+            RuleFor(a => a.DayOfBirth)
+                .GreaterThan(DateTime.MinValue)
+                .WithMessage("Ngày sinh không hợp lệ :(");
+
+            RuleFor(a => a.Gender)
+                .NotEmpty()
+                .WithMessage("Bạn phải chọn giới tính @@");
+
+            RuleFor(a => a.Nation)
+                .NotEmpty()
+                .WithMessage("Dân tộc không được để trống!!!!!")
+                .MaximumLength(50)
+                .WithMessage("Dân tộc tối đa 50 ký tự :3");
+
+            RuleFor(a => a.Information)
+                .MaximumLength(1000)
+                .WithMessage("Thông tin của ca sĩ tối đa 1000 ký tự :3");
+
+        }
+    }
+}
